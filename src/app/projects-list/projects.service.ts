b2a17df24bbc0a430b4a6b67711f357780007d55
omamonaphtali <omamonaphtali@gmail.com>
2018-10-03 22:19:36 +0300

    making it better

--- src/app/projects-list/projects.service.ts
+++ src/app/projects-list/projects.service.ts
@@ -2,9 +2,9 @@ import { ProjectsModel } from "./projects.model";
 
 export class ProjectService {
   projects: ProjectsModel[] = [
-    new ProjectsModel('sample title', 'description of sample project', 'path.to.project', 'path.to.placeholder.location', 'sampleTag| sampleTag 2| sampleTag 3'),
-    new ProjectsModel('sample title', 'description of sample project', 'path.to.project', 'path.to.placeholder.location', 'sampleTag| sampleTag 2| sampleTag 3'),
-    new ProjectsModel('sample title', 'description of sample project', 'path.to.project', 'path.to.placeholder.location', 'sampleTag| sampleTag 2| sampleTag 3'),
+    new ProjectsModel('sample title', 'description of sample project', 'path.to.project', 'https://res.cloudinary.com/jaredomamo/image/upload/v1538587733/practice/work1.jpg', 'sampleTag| sampleTag 2| sampleTag 3'),
+    new ProjectsModel('sample title', 'description of sample project', 'path.to.project', 'https://res.cloudinary.com/jaredomamo/image/upload/v1538587734/practice/work4.jpg', 'sampleTag| sampleTag 2| sampleTag 3'),
+    new ProjectsModel('sample title', 'description of sample project', 'path.to.project', 'https://res.cloudinary.com/jaredomamo/image/upload/v1538587730/practice/work6.jpg', 'sampleTag| sampleTag 2| sampleTag 3'),
     // new ProjectsModel('', '', '', '', ''),
   ];
 
